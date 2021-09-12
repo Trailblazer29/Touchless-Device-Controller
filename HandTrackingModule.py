4c0fb3b05df326db65dbf500064ad1e6fb171f16
Ilham Seladji <43156903+Trailblazer29@users.noreply.github.com>
2021-09-12 15:49:32 +0400

    Handled Keyboard Interrupt exception

--- HandTrackingModule.py
+++ HandTrackingModule.py
@@ -5,14 +5,14 @@ import mediapipe as mp
 
 class HandDetector:
 
-    def __init__(self, image_mode=False, max_hands=2, detection_confidence=0.5, tracking_confidence=0.5):
+    def __init__(self, image_mode=False, max_num_hands=2, detection_confidence=0.5, tracking_confidence=0.5):
         self.image_mode = image_mode
-        self.max_hands = max_hands
+        self.max_num_hands = max_num_hands
         self.detection_confidence = detection_confidence
         self.tracking_confidence = tracking_confidence
-        self.mpHands = mp.solutions.hands
-        self.hands = self.mpHands.Hands(self.image_mode, self.max_hands, self.detection_confidence, self.tracking_confidence)
-        self.mpDraw = mp.solutions.drawing_utils
+        self.mp_hands = mp.solutions.hands
+        self.hands = self.mp_hands.Hands(self.image_mode, self.max_num_hands, self.detection_confidence, self.tracking_confidence)
+        self.drawing = mp.solutions.drawing_utils
 
     def detect_hands(self, img, draw=True):
 
@@ -21,8 +21,8 @@ class HandDetector:
 
         if draw:
             if self.results.multi_hand_landmarks:
-                for handLandmarks in self.results.multi_hand_landmarks:
-                    self.mpDraw.draw_landmarks(img, handLandmarks, self.mpHands.HAND_CONNECTIONS)
+                for hand_landmarks in self.results.multi_hand_landmarks:
+                    self.drawing.draw_landmarks(img, hand_landmarks, self.mp_hands.HAND_CONNECTIONS)
         return img
 
     def get_landmarks(self, img, hand_no=0):
@@ -32,11 +32,7 @@ class HandDetector:
             hand_landmarks = self.results.multi_hand_landmarks[hand_no]
             for lm_id, landmark in enumerate(hand_landmarks.landmark):
                 height, width, channels = img.shape
-                # c: landmark center
-                cx, cy = int(landmark.x * width), int(landmark.y * height)
-                # print(id, cx, cy)
-                # if id == 4:
-                #     cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
+                cx, cy = int(landmark.x * width), int(landmark.y * height) # C: Landmark Center
                 landmarks.append([lm_id, cx, cy])
 
         return landmarks
@@ -47,19 +43,21 @@ def main():
     cap = cv2.VideoCapture(0)
     detector = HandDetector()
     while cap.isOpened():
-        success, img = cap.read()
-        if success:
-            img = detector.detect_hands(img)
-            landmarks = detector.get_landmarks(img)
-            if len(landmarks) != 0:
-                print(landmarks[4])
-            current_time = time.time()
-            fps = 1 / (current_time - previous_time)
-            previous_time = current_time
-            cv2.putText(img, text=str(int(fps)), org=(5, 60), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=2,
-                        color=(144, 12, 63), thickness=3)
-            cv2.imshow("Image", img)
-            cv2.waitKey(1)
+        try:
+            success, img = cap.read()
+            if success:
+                img = detector.detect_hands(img)
+                landmarks = detector.get_landmarks(img)
+                current_time = time.time()
+                fps = 1 / (current_time - previous_time)
+                previous_time = current_time
+                cv2.putText(img, text=str(int(fps)), org=(5, 60), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=2,
+                            color=(144, 12, 63), thickness=3)
+                cv2.imshow("Camera", img)
+                cv2.waitKey(1)
+
+        except KeyboardInterrupt:
+            continue
     cap.release()
     cv2.destroyAllWindows()
 
